48fee3bc0c8dad175d43357b162a50511df50459
Michał Górny <mgorny@gentoo.org>
2012-09-15 23:50:05 +0200

    Ensure that destination file always gets truncated.

--- src/copyfile-copy-regular.c
+++ src/copyfile-copy-regular.c
@@ -24,6 +24,17 @@ copyfile_error_t copyfile_copy_regular(const char* source,
 #ifdef HAVE_POSIX_FALLOCATE
 	int preallocated = 0;
 #endif
+	int open_flags = 0;
+
+	/* if there's no ftruncate(), we need to truncate when opening.
+	 * if there's posix_fallocate(), we truncate anyway trying to get
+	 * a less fragmented space. */
+#ifndef HAVE_FTRUNCATE
+	open_flags |= O_TRUNC;
+#endif
+#ifdef HAVE_POSIX_FALLOCATE
+	open_flags |= O_TRUNC;
+#endif
 
 	fd_in = open(source, O_RDONLY);
 	if (fd_in == -1)
@@ -40,10 +51,18 @@ copyfile_error_t copyfile_copy_regular(const char* source,
 		return COPYFILE_ERROR_OPEN_DEST;
 	}
 
+	/* we can't use posix_fallocate() if we wouldn't be able to truncate
+	 * afterwards. not that any system can really have former without
+	 * the latter; but since autotools does the check already, we can
+	 * use it here too. */
 #ifdef HAVE_POSIX_FALLOCATE
+#	ifdef HAVE_FTRUNCATE
+
 	if (expected_size && !posix_fallocate(fd_out, 0, expected_size))
 		++preallocated;
-#endif
+
+#	endif /*HAVE_FTRUNCATE*/
+#endif /*HAVE_POSIX_FALLOCATE*/
 
 	{
 		off_t offset = 0;
@@ -51,15 +70,11 @@ copyfile_error_t copyfile_copy_regular(const char* source,
 				&offset, expected_size, callback, callback_data);
 		int hold_errno = errno;
 
-#ifdef HAVE_POSIX_FALLOCATE
-		if (preallocated)
+#ifdef HAVE_FTRUNCATE
+		if (ftruncate(fd_out, offset) && !ret)
 		{
-			int trunc_ret = ftruncate(fd_out, offset);
-			if (trunc_ret && !ret)
-			{
-				ret = COPYFILE_ERROR_TRUNCATE;
-				hold_errno = trunc_ret;
-			}
+			ret = COPYFILE_ERROR_TRUNCATE;
+			hold_errno = errno;
 		}
 #endif
 
