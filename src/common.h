2dbdd69670150097f01b60a967a672026ceb46ef
Michał Górny <mgorny@gentoo.org>
2012-09-17 22:01:55 +0200

    Make default group & other mode bits optional.
    These are not defined on win32.

--- src/common.h
+++ src/common.h
@@ -23,9 +23,32 @@ static const int not_reached = 0;
 /* default permissions */
 enum modes
 {
-	perm_dir = S_IRWXU | S_IRWXG | S_IRWXO,
-	perm_file = perm_dir & ~(S_IXUSR | S_IXGRP | S_IXOTH),
-	all_perm_bits = perm_dir | S_ISUID | S_ISGID | S_ISVTX
+	perm_dir = S_IRWXU
+#ifdef S_IRWXG
+		| S_IRWXG
+#endif
+#ifdef S_IRWXO
+		| S_IRWXO
+#endif
+		,
+	perm_file = perm_dir & ~(S_IXUSR
+#ifdef S_IXGRP
+			| S_IXGRP
+#endif
+#ifdef S_IXOTH
+			| S_IXOTH
+#endif
+			),
+	all_perm_bits = perm_dir
+#ifdef S_ISUID
+		| S_ISUID
+#endif
+#ifdef S_ISGID
+		| S_ISGID
+#endif
+#ifdef S_ISVTX
+		| S_ISVTX
+#endif
 };
 
 #endif /*COPYFILE_COMMON_H*/
